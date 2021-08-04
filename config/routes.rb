ee35ac2f44edc5abc67d5ab4191a478445501a5e
nao0725 <aqua.725.times@gmail.com>
2021-08-04 09:07:52 +0000

    Add route.rb to devise for admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+
+  root to: "public/homes#top"
+
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+}
+
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+}
 
 end
