558bdba10e6c88b40f6151d895ef87732a1103da
nao0725 <aqua.725.times@gmail.com>
2021-08-06 07:44:37 +0000

    Add homes/view to image&title

--- app/views/public/articles/index.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.array! @public_articles, partial: "public_articles/public_article", as: :public_article
--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,16 @@ Rails.application.routes.draw do
   get 'public/help' => 'public/homes#help'
   get 'public/home' => 'public/homes#home'
 
-  devise_for :admins, controllers: {
-    sessions:      'admins/sessions',
-    passwords:     'admins/passwords',
-    registrations: 'admins/registrations'
-}
-
   devise_for :users, controllers: {
     sessions:      'users/sessions',
     passwords:     'users/passwords',
     registrations: 'users/registrations'
+}
+
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
 }
 
     resources :articles
