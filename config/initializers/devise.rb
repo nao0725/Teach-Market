52da165479b2b077a27f72df01f943a1c9af476b
nao0725 <aqua.725.times@gmail.com>
2021-08-07 08:09:00 +0000

    Update User to lofin

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -45,8 +45,8 @@ Devise.setup do |config|
   # parameters are used only when authenticating and not when retrieving from
   # session. If you need permissions, you should implement that in a before filter.
   # You can also supply a hash where the value is a boolean determining whether
-  # or not authentication should be aborted when the value is not present.
-  # config.authentication_keys = [:email]
+  # or not authentication should be aborted when the value is not pfresent.
+  config.authentication_keys = [:name]
 
   # Configure parameters from the request object used for authentication. Each entry
   # given should be a request method and it will automatically be passed to the
