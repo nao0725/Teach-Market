140ab59e55a6c686936ea9b3b6fd87e1b3ad23f5
nao0725 <aqua.725.times@gmail.com>
2021-08-08 06:13:07 +0000

    Fix

--- app/assets/javascripts/codeblock.js
+++ app/assets/javascripts/codeblock.js
@@ -1,16 +1,16 @@
 function markdownCode() {
-  const codeInput = document.getElementById('user_answer_code');  // idが'user_answer_code'のもの(textarea)
-  const codeMarkdown = document.getElementById('code_markdown');  // idが'code_markdown'のもの(空のdiv)
+  const codeInput = document.getElementById('user_answer_code');
+  const codeMarkdown = document.getElementById('code_markdown');
   if (codeInput == null || codeMarkdown == null) {
-    return;  // 該当ページでない場合はアーリーリターン
+    return;
   }
 
   codeInput.addEventListener('input', () => {
     const HTML = `${codeInput.value}`;
     codeMarkdown.innerHTML = marked(HTML);
-    const pre_code_nodes = document.querySelectorAll("pre code");  // ここがポイント
-    for(let i = 0; i < pre_code_nodes.length; ++i){                // ここがポイント
-      hljs.highlightBlock(pre_code_nodes[i]);                      // ここがポイント
+    const pre_code_nodes = document.querySelectorAll("pre code");
+    for(let i = 0; i < pre_code_nodes.length; ++i){
+      hljs.highlightBlock(pre_code_nodes[i]);
     }
   });
 };
