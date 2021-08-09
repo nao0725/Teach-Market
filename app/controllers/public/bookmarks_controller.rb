7998972f6f6dc0e338201750cb3f1db5b168491f
nao0725 <aqua.725.times@gmail.com>
2021-08-09 01:39:16 +0000

    Add Views to bookmarks/index

--- app/controllers/public/bookmarks_controller.rb
+++ app/controllers/public/bookmarks_controller.rb
@@ -1,6 +1,10 @@
 class Public::BookmarksController < ApplicationController
   before_action :authenticate_user!
   
+  def index
+    @bookmarks = Bookmark.where(user_id: current_user.id)
+  end
+  
   def create
     @article = Article.find(params[:article_id])
     @bookmark = @article.bookmarks.new(user_id: current_user.id)
