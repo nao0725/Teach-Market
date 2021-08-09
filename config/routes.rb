14077bf9a11d18b742f7253160d5214508486d3e
nao0725 <aqua.725.times@gmail.com>
2021-08-09 02:22:50 +0000

    Update bookmarks/show to fix errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-
   root to: "public/homes#top"
   get '/help' => 'public/homes#help'
   get '/home' => 'public/homes#home'
@@ -19,7 +18,7 @@ Rails.application.routes.draw do
 
   scope module: :public do
      resources :articles do
-       resource :bookmarks, only: [:index, :create, :destroy]
+       resource :bookmarks, only: [:show, :create, :destroy]
      end
   end
 
