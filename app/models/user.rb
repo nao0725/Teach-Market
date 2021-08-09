2a337ff1120a3055232bb5db5b26849eedb9ee06
nao0725 <aqua.725.times@gmail.com>
2021-08-09 00:42:13 +0000

    Add application_controller to authenticate_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :articles, dependent: :destroy
-  
+
   validates :name, presence: true, length: { minimum: 1 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, {presence: true, , format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
+  validates :email, {presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
 
 
 end
