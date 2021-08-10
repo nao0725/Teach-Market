5ba33b0f2eeee49c3b39400c16d7da6ae9e5997c
nao0725 <aqua.725.times@gmail.com>
2021-08-10 08:03:11 +0000

    Update comment/form to star-review

---
+++ db/migrate/20210810080042_add_rateto_comment.rb
@@ -0,0 +1,4 @@
+class AddRatetoComment < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_10_072303) do
+ActiveRecord::Schema.define(version: 2021_08_10_080042) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
