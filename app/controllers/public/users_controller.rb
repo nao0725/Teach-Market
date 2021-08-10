0c394d3e2a67e7108354cdecfa4a8c737aed7391
nao0725 <aqua.725.times@gmail.com>
2021-08-10 01:28:36 +0000

    Add userscontroller&views to settings

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,5 +1,6 @@
 class Public::UsersController < ApplicationController
    before_action :authenticate_user!
+   before_action :set_user
    before_action :correct_user, only: [:edit, :update]
 
   def show
@@ -12,10 +13,14 @@ class Public::UsersController < ApplicationController
   end
 
   private
-
-  def 
+  
+  def set_user
+    @user = User.find(params[:id])
   end
-
-end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to user_path(current_user) unless @user == current_user
+  end
   
 end
