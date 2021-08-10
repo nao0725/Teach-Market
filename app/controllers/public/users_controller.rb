d84ceff87163ec3b29f1e3658a54e1bf4769ee77
nao0725 <aqua.725.times@gmail.com>
2021-08-10 05:47:37 +0000

    Update edit to attachment_image

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -11,9 +11,18 @@ class Public::UsersController < ApplicationController
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
 
   private
+
+  def user_params
+    params.require(:user).permit(:profile_image, :name, :nickname, :introduction)
+  end
 
   def set_user
     @user = User.find(params[:id])
