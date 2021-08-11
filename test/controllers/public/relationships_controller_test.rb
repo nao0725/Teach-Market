6f68225608081c08c9f54c87bc518082d726ea40
nao0725 <aqua.725.times@gmail.com>
2021-08-11 08:49:26 +0000

    [updadte] follow PublicController

--- test/controllers/public/relationships_controller_test.rb
+++ test/controllers/public/relationships_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class RelationshipsControllerTest < ActionDispatch::IntegrationTest
+class Public::RelationshipsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
