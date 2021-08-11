2b0a5a9aa59bc8885066f9c2b0b75a4e0a4358f0
nao0725 <aqua.725.times@gmail.com>
2021-08-11 08:40:58 +0000

    [add] follow アソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,12 @@ class User < ApplicationRecord
   has_many :articles, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+  
+  
 
   #プロフィール画像編集で使用
   attachment :profile_image
