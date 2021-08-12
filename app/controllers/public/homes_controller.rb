6dee8bfd49a5e98026e3a2823074c3faf1030754
nao0725 <aqua.725.times@gmail.com>
2021-08-12 05:16:58 +0000

    [update] レビュー平均順

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -4,9 +4,9 @@ class Public::HomesController < ApplicationController
   end
 
   def home
-    @articles = Article.all.page(params[:page]).per(3).order(params[:sort])
-    @bookmark_ranks = Article.find(Bookmark.group(:article_id).order('count(article_id) desc').limit(10).pluck(:article_id))
-    @review_ranks = Article.find(Comment.group(:article_id).order('count(article_id) desc').limit(10).pluck(:article_id))
+    @articles = Article.all.page(params[:page]).per(3)
+    @bookmarks = Article.find(Bookmark.group(:article_id).order("count(article_id) desc").pluck(:article_id))
+    @reviews = Article.find(Comment.group(:article_id).order("avg(rate) desc").pluck(:article_id))
   end
 
   def help
