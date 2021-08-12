3e3b288be70088b4cb5335d52c6d0c2b30302b70
nao0725 <aqua.725.times@gmail.com>
2021-08-12 12:43:12 +0000

    [fix] 新規登録　nickname

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,6 +34,7 @@ class User < ApplicationRecord
 
   # バリデーション
   validates :name, presence: true, length: { minimum: 1 }
+  validates :nickname, presence: true, length: { minimum: 1 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, {presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  
+    devise_for :users, controllers: {
+    sessions:      "users/sessions",
+    passwords:     "users/passwords",
+    registrations: "users/registrations"
+}
+
+  devise_for :admins, controllers: {
+    sessions:      "admins/sessions",
+    passwords:     "admins/passwords",
+    registrations: "admins/registrations"
+}
+  
   scope module: :public do
     root to: "homes#top"
     get "/help" => "homes#help"
@@ -18,17 +31,5 @@ Rails.application.routes.draw do
    end
 
  end
-
-  devise_for :users, controllers: {
-    sessions:      "users/sessions",
-    passwords:     "users/passwords",
-    registrations: "users/registrations"
-}
-
-  devise_for :admins, controllers: {
-    sessions:      "admins/sessions",
-    passwords:     "admins/passwords",
-    registrations: "admins/registrations"
-}
 
 end
