1f818577f1a03e66b471ee5780137447619e5f5b
nao0725 <aqua.725.times@gmail.com>
2021-08-12 12:01:12 +0000

    [update] js エラー

--- app/assets/javascripts/tab.html.js
+++ app/assets/javascripts/tab.html.js
@@ -1,9 +1,9 @@
-$(("turbolinks:load", function() {
-  let tabs = $(".tab");
+$(document).on("turbolinks:load", function() {
+ let tabs = $(".tab");
   $(".tab").on("click", function() {
     $(".active").removeClass("active");
     $(this).addClass("active");
     const index = tabs.index(this);
     $(".content").removeClass("show").eq(index).addClass("show");
   })
-}))
+})
