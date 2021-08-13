6dc288b0d910112ac1b22fdbb07f9815fb667771
nao0725 <aqua.725.times@gmail.com>
2021-08-13 01:28:33 +0000

    [update] tag search error　解消

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,4 +3,9 @@ class Tag < ApplicationRecord
 has_many :tags, through: :article_tags
 has_many :article_tags
 
+# tag検索できるように設定
+ def self.search(search_word)
+  Tag.where(["tag_name LIKE(?)","%#{search_word}%"])
+ end
+
 end
