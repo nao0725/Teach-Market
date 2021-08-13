b1ddc11ef2d781223d21bc7509827f60d44ca9c1
nao0725 <aqua.725.times@gmail.com>
2021-08-13 08:58:33 +0000

    [add] notifications view

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -1,2 +1,7 @@
 class Public::NotificationsController < ApplicationController
+   def index
+     @notifications =current_user.passive_notifications.page(params[:page]).per(10)
+     @notifications.where(checked: false).each do |notification|
+       notification.update_attributes(checked: true)
+  end
 end
