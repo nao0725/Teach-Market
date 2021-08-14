eff2b599667fb51002c08ea96fba48bc0c0ce81e
nao0725 <aqua.725.times@gmail.com>
2021-08-14 01:55:58 +0000

    [fix] layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def header
+    @notifications = current_user.passive_notifications.all
+    @notification =  @notifications.where.not(visitor_id: current_user.id)
+  end
+
 
   def after_sign_in_path_for(resource)
    home_path(resource)
