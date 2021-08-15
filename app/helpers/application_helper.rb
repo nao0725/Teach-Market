4059548041a3b393a4d13d45ee4e34faca51571e
nao0725 <aqua.725.times@gmail.com>
2021-08-15 05:04:09 +0000

    [fix]　投稿フォーム再修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -35,13 +35,22 @@ module ApplicationHelper
         options = {
             filter_html: true,          #入力されたHTMLをエスケープする
             autolink: true,             #URLをリンクとして表示する
-            space_after_headers: true,  #見出しを作成する
+            space_after_headers: true,  #見出しを作成する# と文字の間にスペースを要求
             no_intra_emphasis: true,    #foo_bar_bazのような文字列のとき強調しない
             fenced_code_blocks: true,   #コードブロックが使える
             tables: true,               #テーブルが記述できる
             hard_wrap: true,            #Markdown内の改行をHTMLの改行にする
             strikethrough: true,        #2つの~で囲まれた箇所を取り消し線にできる
+
             highlight: true,
+            disable_indented_code_blocks: true,
+            lax_spacing: true,
+            superscript: true,
+            underline: true,
+            quote: true,
+            footnotes: true,
+            safe_links_only: true,      #安全なリンクのみ生成する
+
         }
         markdown = Redcarpet::Markdown.new(html_render, options)
         markdown.render(text)
