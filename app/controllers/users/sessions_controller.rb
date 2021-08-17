a5facc04e30856cc58ee239c285ee526ccd9b3df
nao0725 <aqua.725.times@gmail.com>
2021-08-17 05:24:42 +0000

    [fix] 退会機能確認

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -22,9 +22,7 @@ class Users::SessionsController < Devise::SessionsController
   protected
 
   # If you have extra params to permit, append them to the sanitizer.
-   def configure_sign_in_params
-     devise_parameter_sanitizer.permit(:sign_in, keys: [:nickname])
-   end
+
 
    #退会後のログインを阻止
    def reject_user
