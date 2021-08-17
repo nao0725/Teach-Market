8b2817120be566159cf221d79a76391e9471f280
nao0725 <aqua.725.times@gmail.com>
2021-08-17 07:16:16 +0000

    [fix] devise ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,37 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: {
-    sessions:      "users/sessions",
-    passwords:     "users/passwords",
-    registrations: "users/registrations",
+  devise_scope :users do
+    get 'users/sign_in' => 'users/sessions#new', as: 'new_user_session'
+    post 'users/sign_in' => 'users/sessions#create', as: 'user_session'
+    delete 'users/sign_out' => 'users/sessions#destroy', as: 'destroy_user_session'
+    get 'users/sign_up' => 'users/registrations#new', as: 'new_user_registration'
+    post 'users' => 'users/registrations#create', as: 'user_registration'
+    get 'users/password/new' => 'users/passwords#new', as: 'new_user_password'
     omniauth_callbacks: "users/omniauth_callbacks"
     post "users/guest_sign_in', to: 'users/sessions#new_guest"
-}
+  end
 
-  devise_for :admins, controllers: {
-    sessions:      "admins/sessions",
-    passwords:     "admins/passwords",
-    registrations: "admins/registrations"
-}
+#   devise_for :users, controllers: {
+#     sessions:      "users/sessions",
+#     passwords:     "users/passwords",
+#     registrations: "users/registrations",
+#     omniauth_callbacks: "users/omniauth_callbacks"
+#     post "users/guest_sign_in', to: 'users/sessions#new_guest"
+# }
+
+  devise_scope :admins do
+    get 'admins/sign_in' => 'admins/sessions#new', as: 'new_admin_session'
+    post 'admins/sign_in' => 'admins/sessions#create', as: 'admin_session'
+    delete 'admins/sign_out' => 'admins/sessions#destroy', as: 'destroy_admin_session'
+  end
+
+
+#   devise_for :admins, controllers: {
+#     sessions:      "admins/sessions",
+#     passwords:     "admins/passwords",
+#     registrations: "admins/registrations"
+
+# }
 
   scope module: :public do
     root to: "homes#top"
