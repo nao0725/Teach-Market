ab59891dc9a22d98f986efde7b96630dba638078
nao0725 <aqua.725.times@gmail.com>
2021-08-19 05:55:54 +0000

    help layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: [:top]
+  before_action :authenticate_user!, except: [:top, :help]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def header
