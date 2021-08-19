9c4e5a26b8b753f04d04b1973777cc28129b087f
nao0725 <aqua.725.times@gmail.com>
2021-08-19 13:26:48 +0000

    [add] bookmark_count

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_15_082848) do
+ActiveRecord::Schema.define(version: 2021_08_19_132443) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2021_08_15_082848) do
     t.integer "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "bookmark_count", default: 0, null: false
     t.index ["article_id"], name: "index_bookmarks_on_article_id"
     t.index ["user_id"], name: "index_bookmarks_on_user_id"
   end
