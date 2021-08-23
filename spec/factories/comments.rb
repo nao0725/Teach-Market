41c90c7f41adca4238ac185db12d47125314b18e
nao0725 <aqua.725.times@gmail.com>
2021-08-23 02:06:08 +0000

    [add] FactoryBot::Syntax

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -2,5 +2,7 @@ FactoryBot.define do
   factory :comment do
     association :article
     user {article.user }
+    comment_content { Faker::Lorem.characters(number: 10) }
+    rate { Faker::Lorem.characters(number: 3) }
   end
 end
