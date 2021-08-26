e52b8036633c1d9ddeb5170d2e6d1a7a88466e6a
nao0725 <aqua.725.times@gmail.com>
2021-08-26 08:27:57 +0000

    フォロー/通知エラー解消

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -13,13 +13,12 @@ class Public::RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     current_user.follow(params[:user_id])
-    notification = Notification.new()==
+    notification = Notification.new()
     notification.create_follow_notification(current_user, @user.id)
-    # redirect_to request.referer
   end
 
   def destroy
+    @user = User.find(params[:user_id])
     current_user.unfollow(params[:user_id])
-    # redirect_to request.referer
   end
 end
