86288426c124e6f143c4a13a54961f65e16bb65a
nao0725 <aqua.725.times@gmail.com>
2021-08-29 00:21:37 +0000

    [add] 管理者新規登録パス

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -59,4 +59,8 @@ class Admins::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+  
+  def after_sign_up_path_for(resource_or_scope)
+    admins_users_path
+  end
 end
