c8ec46ab06574419845f0265153b3cd25b3f3982
nao0725 <aqua.725.times@gmail.com>
2021-09-15 12:36:14 +0000

    [update]RSpecテスト修正

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,5 +61,16 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
   config.include FactoryBot::Syntax::Methods
+  config.before(:each) do |example|
+    if example.metadata[:type] == :system
+      if example.metadata[:js]
+        driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+      else
+        driven_by :rack_test
+      end
+    end
+  end
+  # Capybara.javascript_driver = :selenium
 end
