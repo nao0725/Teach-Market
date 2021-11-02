7b8a227f5fbc6f7a8cd9ad63b4ccd2a2e4d2baea
nao0725 <aqua.725.times@gmail.com>
2021-11-02 06:37:31 +0000

    [update] db フォロー機能　エラー解消

--- db/schema.rb
+++ db/schema.rb
@@ -81,13 +81,10 @@ ActiveRecord::Schema.define(version: 2021_11_02_061630) do
   end
 
   create_table "relationships", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "follow_id"
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["follow_id"], name: "index_relationships_on_follow_id"
-    t.index ["user_id", "follow_id"], name: "index_relationships_on_user_id_and_follow_id", unique: true
-    t.index ["user_id"], name: "index_relationships_on_user_id"
   end
 
   create_table "tags", force: :cascade do |t|
