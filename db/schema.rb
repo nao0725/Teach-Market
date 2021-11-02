0219e67d3417db2a3a7ed43ef8e495e7e09aa510
nao0725 <aqua.725.times@gmail.com>
2021-11-02 06:20:55 +0000

    [add] statusカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_25_123257) do
+ActiveRecord::Schema.define(version: 2021_11_02_061630) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2021_08_25_123257) do
     t.string "user_id"
     t.string "sub_title"
     t.integer "bookmarks_count", default: 0, null: false
+    t.integer "article_status", default: 0, null: false
   end
 
   create_table "bookmarks", force: :cascade do |t|
@@ -80,10 +81,13 @@ ActiveRecord::Schema.define(version: 2021_08_25_123257) do
   end
 
   create_table "relationships", force: :cascade do |t|
-    t.integer "follower_id"
-    t.integer "followed_id"
+    t.integer "user_id"
+    t.integer "follow_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["follow_id"], name: "index_relationships_on_follow_id"
+    t.index ["user_id", "follow_id"], name: "index_relationships_on_user_id_and_follow_id", unique: true
+    t.index ["user_id"], name: "index_relationships_on_user_id"
   end
 
   create_table "tags", force: :cascade do |t|
